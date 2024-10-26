d0a6885babc84b018456ed38a48f2c57b801b63c
Adam Abubakari <adam.abubakari1@gmail.com>
2024-10-26 20:47:10 +0100

    - Initialised the project - Set up the routing with vue router and implemented unplugin-vue-router for file based routing - Set up Supabase by linking to the remotely hosted account and db, seeded the projects db, created a migration

---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Welcome to the Home View</h1>
+  <RouterLink :to="{ name: '/projects/[slug]', params: { slug: 1 } }"
+    >Go to project 1</RouterLink>
+</template>
---
+++ src/pages/projects/[slug].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <h1>This is the data about the route</h1>
+  <h3>Project Route: {{ route.params.slug }}</h3>
+</template>
